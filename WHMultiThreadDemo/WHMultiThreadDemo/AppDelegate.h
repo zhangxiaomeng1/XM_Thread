28fe75c2f561850bc18579ebd5718858fc47dfac
zhangxiaomeng <zhangxiaomeng@xiangshang360.com>
2018-04-26 17:54:29 +0800

    提交

---
+++ WHMultiThreadDemo/WHMultiThreadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WHMultiThreadDemo
+//  https://github.com/remember17/WHMultiThreadDemo
+//  Created by 吴浩 on 2017/7/6.
+//  Copyright © 2017年 wuhao. All rights reserved.
+//  http://www.jianshu.com/p/7649fad15cdb
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WHMultiThreadDemo/WHMultiThreadDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WHMultiThreadDemo
+//  https://github.com/remember17/WHMultiThreadDemo
+//  Created by 吴浩 on 2017/7/6.
+//  Copyright © 2017年 wuhao. All rights reserved.
+//  http://www.jianshu.com/p/7649fad15cdb 
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
